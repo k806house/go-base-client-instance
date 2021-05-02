2b6d8980f813e5efcb00628a305c4b869de10e35
juliavav <julia.vav@gmail.com>
2021-05-02 04:21:39 +0300

    merge

--- src/pages/GameBoard/components/Help/Help.js
+++ src/pages/GameBoard/components/Help/Help.js
@@ -1,15 +1,10 @@
 import { React, useState } from "react";
-import { Modal, Button } from 'antd';
+import { Modal, Button, Tabs } from "antd";
 import styled from "styled-components";
 import Players from "../GameInfo/components/Players/Players";
-import {
-    HEATMAP_FULL,
-    HEATMAP_ZONE_QUARTER,
-    hints
-} from "./types";
+import { HEATMAP_FULL, HEATMAP_ZONE_QUARTER, hints } from "./types";
 
 const { TabPane } = Tabs;
-
 
 const Wrapper = styled.div`
   width: 46%;
@@ -17,13 +12,12 @@ const Wrapper = styled.div`
 `;
 
 const TabsWrapper = styled.div`
-padding: 10px;
-padding-left: 15px;
-padding-right: 15px;
-.ant-tabs-nav{
+  padding: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+  .ant-tabs-nav {
     margin: 0;
-}
-
+  }
 `;
 
 const HelpWrapper = styled.div`
@@ -57,7 +51,7 @@ const HelpItem = styled.div`
     justify-content: center;
     top: -15px;
     right: 234px;
-    transition: all .3s;
+    transition: all 0.3s;
   }
 
   .button {
@@ -71,12 +65,12 @@ const HelpItem = styled.div`
 
     &:hover {
       transform: scale(1.03);
-     // background: rgba(0,0,0,.13);
-      box-shadow: 0 2px 20px rgba(0,0,0,.15);
+      // background: rgba(0,0,0,.13);
+      box-shadow: 0 2px 20px rgba(0, 0, 0, 0.15);
     }
 
     &:active {
-      transform: scale(.96);
+      transform: scale(0.96);
 
       .badge {
         transform: scale(1.2);
@@ -86,151 +80,210 @@ const HelpItem = styled.div`
 `;
 
 const Help = ({
-    enemyPass,
-    stepColor,
-    yourColor,
-    you,
-    opponent,
-    stepMain,
-    stepTwo,
-    handleHelp,
-    activeHelpId,
-    scores,
-    times,
-    hintCounter,
-    setHintCounter,
-    hint,
-    setGuess
+  enemyPass,
+  stepColor,
+  yourColor,
+  you,
+  opponent,
+  stepMain,
+  stepTwo,
+  handleHelp,
+  activeHelpId,
+  scores,
+  times,
+  hintCounter,
+  setHintCounter,
+  hint,
+  setGuess,
 }) => {
-    const hints_for_choice = hints;
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const hints_for_choice = hints;
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const [curItem, setCurItem] = useState();
+  const [curItem, setCurItem] = useState();
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-        scores && handleHelp(curItem['handleHelp']);
-    };
+  const handleOk = () => {
+    setIsModalVisible(false);
+    scores && handleHelp(curItem["handleHelp"]);
+  };
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
-    return (
-        <Wrapper>
-            <Players
-                enemyPass={enemyPass}
-                opponent={opponent}
-                you={you}
-                stepColor={stepColor}
-                yourColor={yourColor}
-                stepMain={stepMain}
-                stepTwo={stepTwo}
-                times={times}
-            />
-            <Modal title="Угадай-ка!" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <p>А какой ход лучший на твой взгляд, дружище?</p>
-                <input type="text" name="name" onChange={(e) => {
-                    setGuess(e.target.value);
-                }} placeholder="A7"/>
-            </Modal>
+  return (
+    <Wrapper>
+      <Players
+        enemyPass={enemyPass}
+        opponent={opponent}
+        you={you}
+        stepColor={stepColor}
+        yourColor={yourColor}
+        stepMain={stepMain}
+        stepTwo={stepTwo}
+        times={times}
+      />
+      <Modal
+        title="Угадай-ка!"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>А какой ход лучший на твой взгляд, дружище?</p>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => {
+            setGuess(e.target.value);
+          }}
+          placeholder="A7"
+        />
+      </Modal>
 
-            <TabsWrapper>
-                {/*<Tabs type="card" centered>*/}
-                    <Tabs centered>
-                    <TabPane tab="Умные подсказки" key="1">
-                        <HelpWrapper>
-                            {hints_for_choice.map((item) => {
-                                let hintBadgeColor = '2px solid orange';
-                                if (item['fine'] === 2) {
-                                    hintBadgeColor = '2px solid orange';
-                                } else if (item['fine'] === 3) {
-                                    hintBadgeColor = '2px solid red';
-                                } else if (item['fine'] === 1) {
-                                    hintBadgeColor = '2px solid green';
-                                }
+      <TabsWrapper>
+        {/*<Tabs type="card" centered>*/}
+        <Tabs centered>
+          <TabPane tab="Умные подсказки" key="1">
+            <HelpWrapper>
+              {hints_for_choice.map((item) => {
+                let hintBadgeColor = "2px solid orange";
+                if (item["fine"] === 2) {
+                  hintBadgeColor = "2px solid orange";
+                } else if (item["fine"] === 3) {
+                  hintBadgeColor = "2px solid red";
+                } else if (item["fine"] === 1) {
+                  hintBadgeColor = "2px solid green";
+                }
 
-                                return <HelpItem
-                                    active={activeHelpId === item['id']}
-                                    onClick={() => {
-                                        scores && handleHelp(item['handleHelp']);
-                                        if (hintCounter.lastHintStep + 1 === stepMain || hintCounter.lastHintStep === stepMain || stepMain === 0) {
-                                            setHintCounter({ counter: hintCounter.counter + 1, lastHintStep: stepMain });
-                                        } else {
-                                            ;
-                                            setHintCounter({ counter: 0, lastHintStep: -1 });
-                                        }
-                                    }
-                                    }
-                                >
-                                    <div className="button" style={{ border: hintBadgeColor }} onClick={() => {
-                                        showModal();
-                                        setCurItem(item);
-                                        if (item.id > 4) {
-                                            scores && handleHelp(item['handleHelp']);
-                                        }
-                                        if (hintCounter.lastHintStep + 1 === stepMain || hintCounter.lastHintStep === stepMain || stepMain === 0) {
-                                            setHintCounter({ counter: hintCounter.counter + 1, lastHintStep: stepMain });
-                                        } else {
-                                            setHintCounter({ counter: 0, lastHintStep: -1 });
-                                        }
-                                    }}>
-                                        <span className="content">{item['name']}</span>
-                                        <span className="badge" style={{ border: hintBadgeColor }}>{item['fine']}</span>
-                                    </div>
-                                </HelpItem>
-                            })}
-                        </HelpWrapper>
-                    </TabPane>
-                    <TabPane tab="Все подсказки" key="2">
-                        <HelpWrapper>
-                            {hints.map((item) => {
-                                let hintBadgeColor = '2px solid orange';
-                                if (item['fine'] === 2) {
-                                    hintBadgeColor = '2px solid orange';
-                                } else if (item['fine'] === 3) {
-                                    hintBadgeColor = '2px solid red';
-                                } else if (item['fine'] === 1) {
-                                    hintBadgeColor = '2px solid green';
-                                }
-
-                                return <HelpItem
-                                    active={activeHelpId === item['id']}
-                                    onClick={() => {
-                                        scores && handleHelp(item['handleHelp']);
-                                        if (hintCounter.lastHintStep + 1 === stepMain || hintCounter.lastHintStep === stepMain || stepMain === 0) {
-                                            setHintCounter({ counter: hintCounter.counter + 1, lastHintStep: stepMain });
-                                        } else {
-                                            ;
-                                            setHintCounter({ counter: 0, lastHintStep: -1 });
-                                        }
-                                    }
-                                    }
-                                >
-                                    <div className="button" style={{ border: hintBadgeColor }} onClick={() => {
-                                        showModal();
-                                        setCurItem(item);
-                                        if (item.id > 4) {
-                                            scores && handleHelp(item['handleHelp']);
-                                        }
-                                        if (hintCounter.lastHintStep + 1 === stepMain || hintCounter.lastHintStep === stepMain || stepMain === 0) {
-                                            setHintCounter({ counter: hintCounter.counter + 1, lastHintStep: stepMain });
-                                        } else {
-                                            setHintCounter({ counter: 0, lastHintStep: -1 });
-                                        }
-                                    }}>
-                                        <span className="content">{item['name']}</span>
-                                        <span className="badge" style={{ border: hintBadgeColor }}>{item['fine']}</span>
-                                    </div>
-                                </HelpItem>
-                })}
+                return (
+                  <HelpItem
+                    active={activeHelpId === item["id"]}
+                    onClick={() => {
+                      scores && handleHelp(item["handleHelp"]);
+                      if (
+                        hintCounter.lastHintStep + 1 === stepMain ||
+                        hintCounter.lastHintStep === stepMain ||
+                        stepMain === 0
+                      ) {
+                        setHintCounter({
+                          counter: hintCounter.counter + 1,
+                          lastHintStep: stepMain,
+                        });
+                      } else {
+                        setHintCounter({ counter: 0, lastHintStep: -1 });
+                      }
+                    }}
+                  >
+                    <div
+                      className="button"
+                      style={{ border: hintBadgeColor }}
+                      onClick={() => {
+                        showModal();
+                        setCurItem(item);
+                        if (item.id > 4) {
+                          scores && handleHelp(item["handleHelp"]);
+                        }
+                        if (
+                          hintCounter.lastHintStep + 1 === stepMain ||
+                          hintCounter.lastHintStep === stepMain ||
+                          stepMain === 0
+                        ) {
+                          setHintCounter({
+                            counter: hintCounter.counter + 1,
+                            lastHintStep: stepMain,
+                          });
+                        } else {
+                          setHintCounter({ counter: 0, lastHintStep: -1 });
+                        }
+                      }}
+                    >
+                      <span className="content">{item["name"]}</span>
+                      <span
+                        className="badge"
+                        style={{ border: hintBadgeColor }}
+                      >
+                        {item["fine"]}
+                      </span>
+                    </div>
+                  </HelpItem>
+                );
+              })}
             </HelpWrapper>
-        </Wrapper>
-    );
+          </TabPane>
+          <TabPane tab="Все подсказки" key="2">
+            <HelpWrapper>
+              {hints.map((item) => {
+                let hintBadgeColor = "2px solid orange";
+                if (item["fine"] === 2) {
+                  hintBadgeColor = "2px solid orange";
+                } else if (item["fine"] === 3) {
+                  hintBadgeColor = "2px solid red";
+                } else if (item["fine"] === 1) {
+                  hintBadgeColor = "2px solid green";
+                }
+
+                return (
+                  <HelpItem
+                    active={activeHelpId === item["id"]}
+                    onClick={() => {
+                      scores && handleHelp(item["handleHelp"]);
+                      if (
+                        hintCounter.lastHintStep + 1 === stepMain ||
+                        hintCounter.lastHintStep === stepMain ||
+                        stepMain === 0
+                      ) {
+                        setHintCounter({
+                          counter: hintCounter.counter + 1,
+                          lastHintStep: stepMain,
+                        });
+                      } else {
+                        setHintCounter({ counter: 0, lastHintStep: -1 });
+                      }
+                    }}
+                  >
+                    <div
+                      className="button"
+                      style={{ border: hintBadgeColor }}
+                      onClick={() => {
+                        showModal();
+                        setCurItem(item);
+                        if (item.id > 4) {
+                          scores && handleHelp(item["handleHelp"]);
+                        }
+                        if (
+                          hintCounter.lastHintStep + 1 === stepMain ||
+                          hintCounter.lastHintStep === stepMain ||
+                          stepMain === 0
+                        ) {
+                          setHintCounter({
+                            counter: hintCounter.counter + 1,
+                            lastHintStep: stepMain,
+                          });
+                        } else {
+                          setHintCounter({ counter: 0, lastHintStep: -1 });
+                        }
+                      }}
+                    >
+                      <span className="content">{item["name"]}</span>
+                      <span
+                        className="badge"
+                        style={{ border: hintBadgeColor }}
+                      >
+                        {item["fine"]}
+                      </span>
+                    </div>
+                  </HelpItem>
+                );
+              })}
+            </HelpWrapper>
+          </TabPane>
+        </Tabs>
+      </TabsWrapper>
+    </Wrapper>
+  );
 };
 
 export default Help;

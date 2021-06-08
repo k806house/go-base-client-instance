eb2544b889d8c9b3f6608ec814751dc412b950f0
juliavav <julia.vav@gmail.com>
2021-06-08 11:00:18 +1000

    f

--- src/Socket.js
+++ src/Socket.js
@@ -4,7 +4,7 @@ export const token = localStorage.getItem('GoGameToken')
 
 console.log(token)
 
-export const client = new W3CWebSocket('ws://172.104.137.176:41239');
+export const client = new W3CWebSocket('ws://185.22.62.66:41239');
 
 client.onerror = function() {
   console.log('Connection Error');
